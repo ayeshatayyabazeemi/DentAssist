b39abe39b88c3d86fcfcbf6ea48cdcce4e986876
Ayesha Tayyab <ayeshatayyabazeemi@gmail.com>
2025-12-12 17:58:39 +0500

    Saved local changes in develop branch

--- app/Controllers/PatientCardController.php
+++ app/Controllers/PatientCardController.php
@@ -119,8 +119,4 @@ class PatientCardController extends Controller
         $pdf->Output('PatientCard_'.$patientId.'.pdf', 'I');
         exit;
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 23da279fd58c9be3a184acfff278526b71769919
